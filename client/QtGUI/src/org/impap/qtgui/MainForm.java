43b83ec04d9d452b23b4b769ad9c0f5fa65a858c
alert.main <alert.main@0620d6ea-8888-11dd-b6e4-1fb4a74862d0>
2008-11-14 06:59:54 +0000

    append command support added

--- client/QtGUI/src/org/impap/qtgui/MainForm.java
+++ client/QtGUI/src/org/impap/qtgui/MainForm.java
@@ -1,11 +1,12 @@
 package org.impap.qtgui;
 
 import org.imap.common.ApplicationClient;
+import org.imap.common.Logger;
 import org.imap.message.Item;
 import org.imap.message.Message;
 
-import com.trolltech.qt.core.QByteArray;
 import com.trolltech.qt.core.QTimer;
+import com.trolltech.qt.gui.QApplication;
 import com.trolltech.qt.gui.QCloseEvent;
 import com.trolltech.qt.gui.QMainWindow;
 import com.trolltech.qt.gui.QTreeWidgetItem;
@@ -28,7 +29,12 @@ public class MainForm extends QMainWindow {
 		ui.setupUi(this);
 		timer.start();
 		ui.connectButton.clicked.connect(this, "connect(boolean)");
+		ui.newMessageButton.clicked.connect(this, "editMode()");
+		ui.saveMessageButton.clicked.connect(this, "saveMessage()");
 		ui.messageTree.currentItemChanged.connect(this, "itemSelected()");
+
+		client.addLogger(new GuiLogger(ui));
+
 		// menubar.triggered.connect(MainForm, "close()");
 		// connect(timer, SIGNAL())
 	}
@@ -36,6 +42,23 @@ public class MainForm extends QMainWindow {
 	public MainForm(QWidget parent) {
 		super(parent);
 		ui.setupUi(this);
+	}
+
+	public static class GuiLogger implements Logger {
+		private Ui_MainForm ui;
+
+		public GuiLogger(Ui_MainForm ui) {
+			this.ui = ui;
+		}
+
+		public void log(final String str) {
+			QApplication.invokeLater(new Runnable() {
+				public void run() {
+					ui.consoleText.setText(ui.consoleText.toPlainText() + str
+							+ "\n");
+				}
+			});
+		}
 	}
 
 	public void updateMessageTree() {
@@ -52,22 +75,23 @@ public class MainForm extends QMainWindow {
 			}
 
 			client.setChanged(false);
-		}
-		else if((currentMessage != null)&&(ui.messageTree.currentItem() != null)){
+		} else if ((currentMessage != null)
+				&& (ui.messageTree.currentItem() != null)) {
 			Item folder = (Item) ui.messageTree.currentItem().data(1, 0);
-			if (!currentMessage.isDownloaded() && folder.getId().equals(currentMessage.getId())){
-				currentMessage = client.getMessage(folder, currentMessage.getId());
+			if (!currentMessage.isDownloaded()
+					&& folder.getId().equals(currentMessage.getId())) {
+				currentMessage = client.getMessage(folder, currentMessage
+						.getId());
 				ui.fromEdit.setText(currentMessage.from());
 				ui.toEdit.setText(currentMessage.to());
 				ui.subjectEdit.setText(currentMessage.subject());
-				QByteArray byteArray = new QByteArray();
-				byteArray.append(currentMessage.content());
-				ui.bodyText.setContent(byteArray, "text/html");
+				ui.bodyText.setHtml(currentMessage.content());
 			}
 		}
 	}
 
 	private void updateMessageTree(QTreeWidgetItem parent, Item imapItem) {
+		System.out.println("update begin");
 		QTreeWidgetItem item = new QTreeWidgetItem(parent);
 		item.setText(0, imapItem.getText());
 		item.setData(1, 0, imapItem);
@@ -75,6 +99,7 @@ public class MainForm extends QMainWindow {
 		for (Item child : imapItem.getChildren().values()) {
 			updateMessageTree(item, child);
 		}
+		System.out.println("update end");
 	}
 
 	public void connect(boolean value) {
@@ -90,7 +115,36 @@ public class MainForm extends QMainWindow {
 		}
 	}
 
+	public void saveMessage() {
+		if (ui.messageTree.topLevelItem(0) != (ui.messageTree.currentItem()) && ui.messageTree.currentItem() != null) {
+			Item imapItem = (Item) ui.messageTree.currentItem().data(1, 0);
+			client.saveMessage(ui.toEdit.text(), ui.fromEdit.text(), ui.subjectEdit.text(), ui.bodyText.toPlainText(), imapItem);
+		}
+	}
+
+	public void editMode() {
+		if (ui.messageTree.topLevelItem(0) != (ui.messageTree.currentItem()) && ui.messageTree.currentItem() != null) {
+			setViewMode(false);
+			ui.fromEdit.setText(ui.usernameEdit.text());
+			ui.toEdit.setText("");
+			ui.subjectEdit.setText("");
+			ui.bodyText.setText("");
+		}
+	}
+
+	public void viewMode() {
+		setViewMode(true);
+	}
+
+	public void setViewMode(boolean value) {
+		ui.fromEdit.setReadOnly(value);
+		ui.toEdit.setReadOnly(value);
+		ui.subjectEdit.setReadOnly(value);
+		ui.bodyText.setReadOnly(value);
+	}
+
 	public void itemSelected() {
+		viewMode();
 		if (connected) {
 			if (ui.messageTree.currentItem() != null) {
 				Item imapItem = (Item) ui.messageTree.currentItem().data(1, 0);
@@ -101,17 +155,21 @@ public class MainForm extends QMainWindow {
 						ui.fromEdit.setText("");
 						ui.toEdit.setText("");
 						ui.subjectEdit.setText("");
-						ui.bodyText.setContent(new QByteArray(), "text/html");
+						ui.bodyText.setText("");
 						client.listFolder(imapItem);
 						System.out.println("list folder called");
 					} else {
-						currentMessage = client.getMessage(imapItem, imapItem.getId());
+						currentMessage = client.getMessage(imapItem, imapItem
+								.getId());
 						ui.fromEdit.setText(currentMessage.from());
 						ui.toEdit.setText(currentMessage.to());
 						ui.subjectEdit.setText(currentMessage.subject());
-						QByteArray byteArray = new QByteArray();
-						byteArray.append(currentMessage.content());
-						ui.bodyText.setContent(byteArray, "text/html");
+						// QByteArray byteArray = new QByteArray();
+						// byteArray.append(currentMessage.content());
+						String content = currentMessage.content();
+						System.out.println("GUI got message content");
+						ui.bodyText.setHtml(content);
+						System.out.println("content is set");
 					}
 				}
 			}
