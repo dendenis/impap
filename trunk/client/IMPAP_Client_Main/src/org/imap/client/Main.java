5e12e8a67aafeafbedd0863de84acad89e5140e9
alert.main <alert.main@0620d6ea-8888-11dd-b6e4-1fb4a74862d0>
2008-11-27 18:52:43 +0000

    java-mail problem fixed

--- trunk/client/IMPAP_Client_Main/src/org/imap/client/Main.java
+++ trunk/client/IMPAP_Client_Main/src/org/imap/client/Main.java
@@ -3,11 +3,23 @@ package org.imap.client;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
+
 import org.imap.common.CompositeLogger;
 import org.impap.qtgui.ClientGui;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		// add handlers for main MIME types
+		MailcapCommandMap mc = (MailcapCommandMap)CommandMap.getDefaultCommandMap();
+		mc.addMailcap("text/html;; x-java-content-handler=com.sun.mail.handlers.text_html");
+		mc.addMailcap("text/xml;; x-java-content-handler=com.sun.mail.handlers.text_xml");
+		mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain");
+		mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
+		mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822");
+		CommandMap.setDefaultCommandMap(mc);
 		
 		CompositeLogger logger = new CompositeLogger();
 		
